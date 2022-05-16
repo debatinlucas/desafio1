ff03ecad8e103b0ae3946b3b409bcb782a2dab5e
Lucas Debatin <lucasdebatin@univali.br>
2022-05-16 15:19:43 -0300

    CM - alterado a mensagem

--- src/main/java/br/univali/desafio1/Desafio1.java
+++ src/main/java/br/univali/desafio1/Desafio1.java
@@ -12,6 +12,6 @@ package br.univali.desafio1;
 public class Desafio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Lucas Debatin");
     }
 }
